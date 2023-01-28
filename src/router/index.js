fc836197ba135475d5c638a44f8063674aa3cde6
Embrace <2069814988@qq.com>
2023-01-28 22:49:22 +0800

    fix:xxx

--- src/router/index.js
+++ src/router/index.js
@@ -228,4 +228,10 @@ const router = new VueRouter({
     ]
 })
 
+// 防止连续点击多次路由报错
+let routerPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+    return routerPush.call(this, location).catch(err => err)
+}
+
 export default router
